d98b53118610f3d6f80e4535de341bc23d3a4264
hithaishy <Hithaishy.jc@gmail.com>
2018-10-24 23:01:09 -0700

    adding + - buttons and some transitions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     body.addEventListener('click', (event) => {             //adding event listener once the DOM is loaded
         if (event.target && event.target.nodeName === 'BUTTON') {       //Listening to all click events on td
             let content = event.target.nextElementSibling;
+            event.target.classList.toggle('is-open');
             if(content.style.maxHeight){
                 //that means the accordion is open so close
                 content.style.maxHeight = null;

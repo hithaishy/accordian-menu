cfea06e116d19612b88207a672c27e075b609481
hithaishy <Hithaishy.jc@gmail.com>
2018-10-24 23:06:45 -0700

    package.json

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.addEventListener("DOMContentLoaded", () => {
-    let body = document.getElementById('body');
-    body.addEventListener('click', (event) => {             //adding event listener once the DOM is loaded
-        if (event.target && event.target.nodeName === 'BUTTON') {       //Listening to all click events on td
-            let content = event.target.nextElementSibling;
-            event.target.classList.toggle('is-open');
-            if(content.style.maxHeight){
-                //that means the accordion is open so close
-                content.style.maxHeight = null;
-            } else {
-                //open the accordion
-                content.style.maxHeight = content.scrollHeight + "px";
+(() => {
+    document.addEventListener("DOMContentLoaded", () => {
+        let body = document.getElementById('body');
+        body.addEventListener('click', (event) => {             //adding event listener once the DOM is loaded
+            if (event.target && event.target.nodeName === 'BUTTON') {       //Listening to all click events on td
+                let content = event.target.nextElementSibling;
+                event.target.classList.toggle('is-open');
+                if (content.style.maxHeight) {
+                    //that means the accordion is open so close
+                    content.style.maxHeight = null;
+                } else {
+                    //open the accordion
+                    content.style.maxHeight = content.scrollHeight + "px";
+                }
             }
-        }
+        });
     });
-});
+})();

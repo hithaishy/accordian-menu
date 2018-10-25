a75bbb76763e8b09cc167cf20e82788d13921f33
hithaishy <Hithaishy.jc@gmail.com>
2018-10-24 22:48:39 -0700

    display and hide accordion items

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let body = document.getElementById('body');
+    body.addEventListener('click', (event) => {             //adding event listener once the DOM is loaded
+        if (event.target && event.target.nodeName === 'BUTTON') {       //Listening to all click events on td
+            let content = event.target.nextElementSibling;
+            if(content.style.maxHeight){
+                //that means the accordion is open so close
+                content.style.maxHeight = null;
+            } else {
+                //open the accordion
+                content.style.maxHeight = content.scrollHeight + "px";
+            }
+        }
+    });
+});
